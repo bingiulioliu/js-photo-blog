d8978d88d2169a31ca633b5d56ec4db35d0f4210
Bin Giulio Liu <bingiulio.liu@gmail.com>
2026-04-17 13:08:30 +0200

    ADD card injector function

--- script/script.js
+++ script/script.js
@@ -2,7 +2,31 @@
 'use strict';
 
 // DOM
-const cardsContainerHtmlEl = document.querySelector('#cards-container');
+const cardsContainerEl = document.querySelector('#cards-container');
 const API_URL = 'https://lanciweb.github.io/demo/api/pictures/';
 
 
+// Setto i parametri
+/** 
+* @param {{id: number, title: string, date: string, url: string} } cards
+*/
+// Funzione per iniettare la card ad ogni giro
+function cardInj(cards) {
+    // Variabile di appoggio
+    let cardHtml = '';
+    // Ciclo for
+    for (const card of cards) {
+        cardHtml += `
+        <div class="card" style="width: 18rem;">
+            <img src="${card.url}" class="card-img-top" alt="${card.title}">
+            <div class="card-body">
+                <h5 class="card-title">${card.title}</h5>
+                <p class="card-text">${card.date}</p>
+            </div>
+        </div>
+        `
+    }
+    if (cardsContainerEl !== null){
+        cardsContainerEl.innerHTML = cardHtml;
+    }
+}
