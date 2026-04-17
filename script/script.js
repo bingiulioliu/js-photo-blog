5c16e38f3510eb8d5dd34ab9e8c1beb6eead9ab2
Bin Giulio Liu <bingiulio.liu@gmail.com>
2026-04-17 15:26:05 +0200

    ADD Fetch

--- script/script.js
+++ script/script.js
@@ -4,11 +4,12 @@
 // DOM
 const cardsContainerEl = document.querySelector('#cards-container');
 const API_URL = 'https://lanciweb.github.io/demo/api/pictures/';
-
+const loadingMsgEl = document.querySelector('#loadingMsg');
+const errorMsgEl = document.querySelector('#errorMsg');
 
 // Setto i parametri
 /** 
-* @param {{id: number, title: string, date: string, url: string} } cards
+* @param {{id: number, title: string, date: string, url: string}[] } cards
 */
 // Funzione per iniettare la card ad ogni giro
 function cardInj(cards) {
@@ -20,8 +21,8 @@ function cardInj(cards) {
         <div class="card" style="width: 18rem;">
             <img src="${card.url}" class="card-img-top" alt="${card.title}">
             <div class="card-body">
-                <h5 class="card-title">${card.title}</h5>
                 <p class="card-text">${card.date}</p>
+                <p class="card-title">${card.title}</p>
             </div>
         </div>
         `
@@ -30,3 +31,24 @@ function cardInj(cards) {
         cardsContainerEl.innerHTML = cardHtml;
     }
 }
+
+// Fetch
+if (loadingMsgEl !== null && errorMsgEl){
+    // Rendo visibile il messaggio
+    loadingMsgEl.classList.remove('d-none');
+    // Inizio il codice asincrono di promesse
+    fetch (API_URL)
+        .then(response=>{
+            return response.json();
+        })
+        .then(json =>{
+            cardInj(json);
+        })
+        .catch(error =>{
+            console.error(Error);
+            errorMsgEl.classList.remove('d-none');
+        })
+        .finally(() => {
+            loadingMsgEl.classList.add('d-none');
+        })        
+};
